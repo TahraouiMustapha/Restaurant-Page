7a4a71eeb7b8ed689c457492e0413ea588bd4404
mustapha <moustaphaoussama1818@gmail.com>
2024-04-16 09:02:29 +0100

    Set up an html skelton with 3 btns in the header , Setting our webpack configuration to devlopement mode

---
+++ src/index.js
@@ -0,0 +1,1 @@
+console.log('hi moh jamal');
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,16 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        watchFiles: ['src/**/*.php', 'public/**/*'],
     },
     plugins: [
         new HtmlWebpackPlugin({
