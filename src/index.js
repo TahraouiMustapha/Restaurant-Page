d34230323cb4ce163efb87862dc9c5e2f0406d17
mustapha <moustaphaoussama1818@gmail.com>
2024-04-18 17:22:13 +0100

    Appending each new element to my content div , loading image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('hi moh jamal');
+import image from './image.jpg';
+
+const content = document.querySelector('#content');
+
+const headLine = document.createElement('h2');
+headLine.textContent = 'Welcome in our Restaurant';
+content.appendChild(headLine);
+
+const sometxt = document.createElement('p');
+sometxt.textContent = 'some text about how wonderful the restaurant is';
+content.appendChild(sometxt);
+
+const myImage = new Image();
+myImage.src = image;
+myImage.width = 120;
+content.appendChild(myImage);
+
--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,16 @@ module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules:[
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            }
+    ]
     },
     devtool: 'inline-source-map',
     devServer: {
