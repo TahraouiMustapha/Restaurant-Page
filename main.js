e2a1e3f46e86be6b6279cc131e7dc5f01495acf3
mustapha <moustaphaoussama1818@gmail.com>
2024-04-22 18:57:32 +0100

    Initial dist subtree commit

---
+++ main.js
@@ -0,0 +1,20 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const content = document.querySelector('#content');
+
+const headLine = document.createElement('h2');
+headLine.textContent = 'Welcome in our Restaurant';
+content.appendChild(headLine);
+
+const sometxt = document.createElement('p');
+sometxt.textContent = 'some text about how wonderful the restaurant is';
+content.appendChild(sometxt);
+
+console.log('hh');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZXN0YXVyYW50LXBhZ2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgY29udGVudCA9IGRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoJyNjb250ZW50Jyk7XHJcblxyXG5jb25zdCBoZWFkTGluZSA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2gyJyk7XHJcbmhlYWRMaW5lLnRleHRDb250ZW50ID0gJ1dlbGNvbWUgaW4gb3VyIFJlc3RhdXJhbnQnO1xyXG5jb250ZW50LmFwcGVuZENoaWxkKGhlYWRMaW5lKTtcclxuXHJcbmNvbnN0IHNvbWV0eHQgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KCdwJyk7XHJcbnNvbWV0eHQudGV4dENvbnRlbnQgPSAnc29tZSB0ZXh0IGFib3V0IGhvdyB3b25kZXJmdWwgdGhlIHJlc3RhdXJhbnQgaXMnO1xyXG5jb250ZW50LmFwcGVuZENoaWxkKHNvbWV0eHQpO1xyXG5cclxuY29uc29sZS5sb2coJ2hoJyk7XHJcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
